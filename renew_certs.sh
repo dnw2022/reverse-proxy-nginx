8df92216c17485a1073b4b22086232564ec5e992
Jeroen Bijlsma <jeroen_bijlsma@yahoo.com>
2022-04-06 13:56:55 +0200

    Fix issue with wildcard certs

--- renew_certs.sh
+++ renew_certs.sh
@@ -8,14 +8,18 @@ expiring="Certificate will expire"
 # If there is a certificate, check if it will expire soon
 if [[ ! -z "$CERT_PFX_BASE64" ]]
 then
-  echo "$CERT_PFX_BASE64" | base64 --decode > $CERT_DOMAIN.pfx
+  echo "$CERT_PFX_BASE64" | base64 --decode > cert.pfx
 
-  openssl pkcs12 -in $CERT_DOMAIN.pfx -out $CERT_DOMAIN.pem -nodes -password pass:$CERT_PWD
+  openssl pkcs12 -in cert.pfx -out cert.pem -nodes -password pass:$CERT_PWD
 
   endInDays=45
   let endInSeconds=endInDays*24*60*60
 
-  expiring=$(openssl x509 -in $CERT_DOMAIN.pem -checkend $endInSeconds)
+  expiring=$(openssl x509 -in cert.pem -checkend $endInSeconds)
+
+  echo $expiring
+
+  rm ./cert.pem
 fi
 
 # Renew if certificate is expiring soon
@@ -48,24 +52,24 @@ then
   sudo chmod -R 777 ./*
 
   echo "Create a PFX for Azure"
-  openssl pkcs12 -inkey ./live/$CERT_DOMAIN/privkey.pem -in live/$CERT_DOMAIN/fullchain.pem -export -out $CERT_DOMAIN.pfx -passout pass:$CERT_PWD
+  DOMAIN_NAME=$(echo $CERT_DOMAIN | sed -e "s/\*.//g")
+  openssl pkcs12 -inkey ./live/$DOMAIN_NAME/privkey.pem -in live/$DOMAIN_NAME/fullchain.pem -export -out cert.pfx -passout pass:$CERT_PWD
 
-  sudo chmod 777 $CERT_DOMAIN.pfx
+  sudo chmod 777 cert.pfx
 
-  FINGER_PRINT=$(openssl pkcs12 -in ./$CERT_DOMAIN.pfx -nodes -password pass:$CERT_PWD | openssl x509 -noout -fingerprint | sed -e "s/\SHA1 Fingerprint=//g" -e "s/\://g")
+  FINGER_PRINT=$(openssl pkcs12 -in ./cert.pfx -nodes -password pass:$CERT_PWD | openssl x509 -noout -fingerprint | sed -e "s/\SHA1 Fingerprint=//g" -e "s/\://g")
   
   # https://docs.github.com/en/enterprise-cloud@latest/actions/security-guides/encrypted-secrets
-  cat $CERT_DOMAIN.pfx | base64 > $CERT_DOMAIN.base64
+  cat cert.pfx | base64 > cert.base64
 
   echo "Store thumprint and pfx file as github secrets"
   gh secret set CERT_THUMBPRINT --body "$FINGER_PRINT"
-  gh secret set CERT_PFX_BASE64 < $CERT_DOMAIN.base64
+  gh secret set CERT_PFX_BASE64 < cert.base64
 
   echo "Cleanup Files"
   rm ./cloudflare.ini
-  rm ./$CERT_DOMAIN.pfx
-  rm ./$CERT_DOMAIN.pem
-  rm ./$CERT_DOMAIN.base64
+  rm ./cert.pfx
+  rm ./cert.base64
 
   # gh workflow run azure-app-service-deploy.yml --ref master
 else
